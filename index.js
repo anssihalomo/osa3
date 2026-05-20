b6cd9904fa4d01db0cf54282602374a6d0ef312b
anssihalomo <anssi.halomo12@gmail.com>
2026-05-20 11:22:38 +0300

    lisätty frontend

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(cors())
-
 app.use(express.json())
+
+app.use(express.static('dist'))
 
 morgan.token('body', (request) => {
     if (request.method === 'POST') {
